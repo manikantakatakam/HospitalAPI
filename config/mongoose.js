85e3b2e5bf9a1f47c62c7d2d51eb3f28dec37755
manikantakatakam <90960975+manikantakatakam@users.noreply.github.com>
2023-06-03 20:09:27 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost:27017/hospitalAPI', {
+    // useCreateIndex: true,
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    
+    
+}).then(() => {
+    console.log('connection is successfully');
+}).catch((e) => {
+    console.log('No Connection');
+})
+
