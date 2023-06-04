a5a9459fb0e775fe727db73904d037c66dbd7cb8
manikantakatakam <90960975+manikantakatakam@users.noreply.github.com>
2023-06-04 08:31:04 +0530

    latest

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res) {
     }
 )});
 
-router.use('/doctors', require('./doctor')); //routes to all doctors reuqest
+router.use('/doctors', require('./doctor')); //routes to all doctors request
 router.use('/patients', require('./patient')); //routes to all pateints request
 router.use('/reports', require('./report')); //routes to all reports request
 
